9b4a3fd8643bd1fabb34b5e3db56b80f46f87794
Jerry Ray Nelson III <91078297+jerryraynelson3@users.noreply.github.com>
2023-05-06 11:22:40 -0600

    I did a thing

--- foodInventorySystem.cpp
+++ foodInventorySystem.cpp
@@ -1,12 +1,43 @@
 #include <windows.h>
 #include <tchar.h>
 
+typedef struct tagWNDCLASSEXW {
+	UINT      cbSize;
+	UINT      style;
+	WNDPROC   lpfnWndProc;
+	int       cbClsExtra;
+	int       cbWndExtra;
+	HINSTANCE hInstance;
+	HICON     hIcon;
+	HCURSOR   hCursor;
+	HBRUSH    hbrBackground;
+	LPCWSTR   lpszMenuName;
+	LPCWSTR   lpszClassName;
+	HICON     hIconSm;
+} WNDCLASSEXW, * PWNDCLASSEXW, * NPWNDCLASSEXW, * LPWNDCLASSEXW;
+
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR     lpCmdLine,
-	_In_ int       nCmdShow
-);
+	_In_ int       nCmdShow);
+ {
+
+	new WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+};
 
 LRESULT CALLBACK WndProc(
 	_In_ HWND   hWnd,
